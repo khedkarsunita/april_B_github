4ff9283ce90815d8b6dbea9c9bf419fdab7fd11b
Admin <Admin@DESKTOP-A4AQJ8J>
2021-08-01 22:47:45 +0530

    [assign new code]

--- src/april_B/demo1.java
+++ src/april_B/demo1.java
@@ -1,5 +1,10 @@
 package april_B;
 
 public class demo1 {
-
+public static void main(String[] args) {
+	
+	System.out.println("hello");
+	System.out.println(" how are you");
+	System.out.println("i am good");
+}
 }
---
+++ src/april_B/demo2.java
@@ -0,0 +1,11 @@
+package april_B;
+
+public class demo2 {
+public static void main(String[] args) {
+	
+	System.out.println("sunita");
+	System.out.println("khedkar");
+	
+	
+}
+}
